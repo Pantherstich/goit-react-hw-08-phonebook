bb264113767a0639d6f6a5fb0844042669f52ce7
Pantherstich <132139666+Pantherstich@users.noreply.github.com>
2024-01-09 18:18:38 +0200

    tolate to be perfect

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -3,10 +3,14 @@ import { ContactForm } from 'components/ContactForm/ContactForm';
 import { ContactList } from 'components/ContactList/ContactList';
 import { Filter } from 'components/Filter/Filter';
 import { Div } from 'components/Layout/Layout.styled';
+import { StyledLink } from 'components/RegistrForm/RegistrForm.styled';
 
 const ContactsPage = () => {
   return (
     <Div>
+      <StyledLink type="button" to="/">
+        Home Page
+      </StyledLink>
       <ContactForm />
       <h2>Contacts</h2>
       <Filter />
